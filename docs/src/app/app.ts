b2cde22ea33423dc29cd27b79818ba998e7aaecd
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2025-12-29 07:27:18 -0500

    feat(docs): Add angular-fontawesome for icons
    - Installed @fortawesome/angular-fontawesome and icon packages
    - Replaced inline SVGs with FontAwesome icons in sidebar
    - Added Rust brand icon for logo
    - Added icons for all navigation items (Home, Rocket, Code, etc.)
    - Added GitHub icon in footer with external link indicator
    - Added 'Made with heart' footer text

--- docs/src/app/app.ts
+++ docs/src/app/app.ts
@@ -1,14 +1,57 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { 
+  faGithub, 
+  faRust 
+} from '@fortawesome/free-brands-svg-icons';
+import { 
+  faBars, 
+  faBook, 
+  faCode, 
+  faCog, 
+  faFileAlt, 
+  faHome, 
+  faRocket, 
+  faShieldAlt, 
+  faTachometerAlt,
+  faTerminal,
+  faDatabase,
+  faFolderOpen,
+  faWindowMaximize,
+  faExternalLinkAlt,
+  faHeart
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, FontAwesomeModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   sidebarOpen = false;
+
+  // Brand icons
+  faGithub = faGithub;
+  faRust = faRust;
+
+  // Solid icons
+  faBars = faBars;
+  faBook = faBook;
+  faCode = faCode;
+  faCog = faCog;
+  faFileAlt = faFileAlt;
+  faHome = faHome;
+  faRocket = faRocket;
+  faShieldAlt = faShieldAlt;
+  faTachometerAlt = faTachometerAlt;
+  faTerminal = faTerminal;
+  faDatabase = faDatabase;
+  faFolderOpen = faFolderOpen;
+  faWindowMaximize = faWindowMaximize;
+  faExternalLinkAlt = faExternalLinkAlt;
+  faHeart = faHeart;
 
   toggleSidebar() {
     this.sidebarOpen = !this.sidebarOpen;
